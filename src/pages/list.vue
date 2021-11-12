ebef3fc1da4b0bbe7ed106b4e94264382f508628
Markus Holzer <markus.holzer@hytech.at>
2021-11-12 17:39:43 +0100

    list of characters

--- src/pages/list.vue
+++ src/pages/list.vue
@@ -3,7 +3,7 @@
     <!-- content -->
 
     <q-table
-      title="Treats"
+      title="Characters"
       :rows="rows"
       :columns="columns"
       row-key="name"
@@ -13,142 +13,39 @@
 </template>
 
 <script>
-const columns = [
+
+
+import axios from 'axios';
+export default {
+  async created() {
+    console.log('created.');
+    var url = 'https://www.breakingbadapi.com/api/characters/';
+    this.$axios.get(url).then(response => {
+      this.rows = response.data;
+    })
+
+    this.columns = [
   {
     name: 'name',
     required: true,
-    label: 'Dessert (100g serving)',
+    label: 'Name',
     align: 'left',
     field: row => row.name,
     format: val => `${val}`,
     sortable: true
   },
-  { name: 'calories', align: 'center', label: 'Calories', field: 'name', sortable: true }
-]
+  { name: 'birthday', align: 'left', label: 'Birthday', field: 'birthday', sortable: true },
+  { name: 'calories', align: 'left', label: 'Nickname', field: 'nickname', sortable: true }
+  ]
 
-const rows = [
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: '8%',
-    iron: '1%'
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: '6%',
-    iron: '7%'
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: '3%',
-    iron: '8%'
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: '7%',
-    iron: '16%'
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: '0%',
-    iron: '0%'
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: '0%',
-    iron: '2%'
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: '0%',
-    iron: '45%'
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: '2%',
-    iron: '22%'
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: '12%',
-    iron: '6%'
-  }
-]
-
-import axios from 'axios';
-export default {
-  // name: 'PageName',
-  async created() {
-    console.log('created.');
-    var url = 'https://www.breakingbadapi.com/api/characters/';
-    const result = await this.$axios.get(url)
-    console.log(result);
-    this.rows = result.data;
-    console.log(this.characters);
+    
+    console.log(this.rows);
   }, 
-  setup () {
-    return {
-      columns,
-      rows
-    }
-  },
+  
   data () {
     return {
-      
+      rows: [],
+      columns: [],
       characters: ''
     }
   }
