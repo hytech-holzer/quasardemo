eb5a9e982122cf94352ceef3e138997cc465416a
Markus Holzer <markus.holzer@hytech.at>
2021-11-12 16:14:04 +0100

    layout, page, router

---
+++ src/layouts/search.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <q-header elevated>
+      <q-tabs>
+        <q-route-tab :to="{name: 'profile', params: {id: 1}}" replace label="Walter White" />
+        <q-route-tab :to="{name: 'profile', params: {id: 2}}" replace label="Jesse Pinkman" />
+        <q-route-tab :to="{name: 'profile', params: {id: 'random'}}" replace label="Random" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view :key="$route.params.id"/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  // name: 'LayoutName',
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/profile.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page padding>
+    <div class="column justify-center items-center">
+    <h3>{{character.name}}</h3>
+    <q-card class="my-card">
+      <img :src="character.img" >
+      <q-card-section>
+        <div class="text-h6">{{character.name}}</div>
+        <div class="text-subtitle2">{{character.nickname}}</div>
+      </q-card-section>
+    </q-card>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+  .my-card {
+    width: 25%;
+  }
+</style>
+
+<script>
+import axios from 'axios';
+export default {
+  // name: 'PageName',
+  async created() {
+    console.log('created.');
+    console.log(this.$route.params.id);
+    var url = 'https://www.breakingbadapi.com/api/characters/' + this.$route.params.id;
+    if (this.$route.params.id === 'random') {
+      url = 'https://www.breakingbadapi.com/api/character/random';
+    }
+    const result = await this.$axios.get(url)
+    console.log(result);
+    this.character = result.data[0];
+  }, 
+  data () {
+    return {
+      character: ''
+    }
+  }
+}
+</script>
